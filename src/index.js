4e2343d2e82ae22d35174eeed4cdc8576899fa4a
Alex <mathewalex806@gmail.com>
2024-03-20 08:16:02 +0530

    middleware

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ const app = express();
 
 const PORT = 3001;
 
+//Middleware used to process POST requests
+
+app.use(express.json());
+app.use(express.urlencoded());
+
 const goList = [
     {
     item: "milk",
@@ -25,13 +30,28 @@ const goList = [
 app.listen(PORT, ()=> console.log("Running express server on port 3001"));
 
 
-app.get('/groceries', (req, res)=> {
+app.get('/groceries', 
+(req, res, next) => 
+{
+    console.log("Before handling the request");
+    next();
+},
+(req, res, next)=> 
+{
     res.send(goList);
-});
+    next();
+},
+()=>
+{
+    console.log("Finished execution");
+}
+
+);
 
 app.post('/groceries', (req, res) =>
 {
     console.log(req.body);
+    goList.push(req.body);
     res.send(201);
 
 });
